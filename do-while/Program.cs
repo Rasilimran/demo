b1e0c200c819ba20ac396850c80931069afd8803
Rasilimran <rasilimran828@gmail.com>
2024-06-10 16:24:03 +0530

    do while

---
+++ do-while/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("menu");
+            //Console.WriteLine("1.add");
+            //Console.WriteLine("2.sub");
+            //while (true)
+            //{
+            //    int n1 = int.Parse(Console.ReadLine());
+            //    int n2 = int.Parse(Console.ReadLine());
+            //    int input = int.Parse(Console.ReadLine());
+            //    if (input == 1)
+            //    {
+            //        Console.WriteLine(n1 + n2);
+            //    }
+            //    else if (input == 2)
+            //    {
+            //        Console.WriteLine(n1 - n2);
+            //    }
+            //    else if (input == 2)
+            //    {
+            //        Console.WriteLine(n1 - n2);
+            //    }
+            //    string msg = Console.ReadLine();
+            //    if (msg == "yes")
+            //    {
+            //        Console.WriteLine("1.yes to be continue");
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+            //}
+
+
+            //Console.WriteLine("rock");
+            //Console.WriteLine("paper");
+            //Console.WriteLine("scissor");
+            //while (true)
+            //{
+            //    int userchoice = int.Parse(Console.ReadLine());
+            //    Random r = new Random();
+            //    int cc = r.Next(1, 4);
+            //    if (userchoice == cc)
+            //    {
+            //        Console.WriteLine("draw");
+            //    }
+            //    else if (userchoice == 1 && cc == 3)
+            //    {
+            //        Console.WriteLine("user");
+            //    }
+            //    else if (userchoice == 2 && cc == 1)
+            //    {
+            //        Console.WriteLine("user");
+            //    }
+            //    else if (userchoice == 3 && cc == 2)
+            //    {
+            //        Console.WriteLine("user win");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("cc");
+            //    }
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
+        
+    
+
